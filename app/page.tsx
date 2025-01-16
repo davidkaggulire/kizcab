fc77796cd7c75a75dec7f125c8d0b3e4025e41fe
davidkaggulire <dkaggulire@gmail.com>
2025-01-16 21:16:46 +0100

    change from portfolio to services

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 // import Image from "next/image";
 import Header from "./header/page";
 import About from "./section/page";
-import Portfolio from "./portfolio/page";
+import Services from "./services/page";
 import Contact from "./contact/page";
 import Footer from "./footer/page";
-
-
 
 export default function Home() {
   return (
@@ -13,11 +11,10 @@ export default function Home() {
       <Header />
       <main>
         <About />
-        <Portfolio />
+        <Services />
         <Contact />
       </main>
       <Footer />
     </div>
-  )
-
+  );
 }
